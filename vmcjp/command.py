d6239491884fb8b4033e4e8f235f81575ff28d17
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-12 23:12:34 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -10,7 +10,25 @@ def event_handler(event):
     eval(event.get("vmc_command"))(event)
 
 def list_sddcs(event):
-    return sddc_list(
-        event.get("token"), 
-        event.get("org_id")
-    )
+    if check_access_token(
+        event.get("expire_time")
+    ):
+        sddcs = {
+            "sddc_list": sddc_list(
+                event.get("access_token"), 
+                event.get("org_id")
+            )
+        }
+    else:
+        access_token = login(event.get("token")
+        sddcs = {
+            "sddc_list": sddc_list(
+                access_token, 
+                event.get("org_id")
+            )
+        }
+        sddcs.update(
+            {
+                "access_token": access_token
+        )
+    return sddcs
