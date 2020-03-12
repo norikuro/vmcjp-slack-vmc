34bbef2577e18b0d1dc356b1a73c1202cbeb7e43
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-13 08:38:54 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -5,14 +5,20 @@ from vmcjp.vmc.vmc_client import login, sddc_list
 
 def lambda_handler(event):
     expire_time = event.get("expire_time")
-    if expire_time is not None and expired(expire_time):
+    command = event.get("vmc_command")
+    
+    if "list_sddcs" is in command:
         data = login(event.get("token"))
         event.update(data)
-        write_cred_db(
-            event.get("db_url"),
-            event.get("user_id"),
-            data
-        )        
+    else:
+        if expire_time is not None and expired(expire_time):
+            data = login(event.get("token"))
+            event.update(data)
+            write_cred_db(
+                event.get("db_url"),
+                event.get("user_id"),
+                data
+            )
     eval(event.get("vmc_command"))(event)
 
 def expired(expire_time):
