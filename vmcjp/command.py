b4326cae576bcb174cf20a7bc3114c8e3d54d9e6
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-13 08:35:56 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -4,7 +4,8 @@ from vmcjp.slack.db import write_cred_db
 from vmcjp.vmc.vmc_client import login, sddc_list
 
 def lambda_handler(event):
-    if expired(event.get("expire_time")):
+    expire_time = event.get("expire_time")
+    if expire_time is not None and expired(expire_time):
         data = login(event.get("token"))
         event.update(data)
         write_cred_db(
