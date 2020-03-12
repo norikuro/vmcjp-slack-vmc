0493a2545cd372e77d814b07c4f253a8101fc2b8
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-12 22:44:07 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -10,15 +10,7 @@ def event_handler(event):
     eval(cmd)(event, db)
 
 def list_sddcs(event, db):
-    message_handler(msg_const.SDDCS_TXT, event)
-    event.update(
-        {
-#            "sddcs": list_sddcs_(
-            "sddcs": sddc_list(
-#                event.get("token"), 
-                event.get("access_token"), 
-                event.get("org_id")
-            )
-        }
+    return sddc_list(
+        event.get("access_token"), 
+        event.get("org_id")
     )
-    message_handler(msg_const.SDDCS_MSG, event)
