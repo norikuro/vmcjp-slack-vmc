a5f8ae20a6c44e229da177ba6d83b742e9721e3a
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-13 16:11:55 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -8,15 +8,13 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    logging.info(event)
+#    logging.info(event)
     expire_time = event.get("expire_time")
     command = event.get("vmc_command")
     
     if "list_sddcs" in command:
-        logging.info("!!!next is login and update event")
         data = login(event.get("token"))
         event.update(data)
-        logging.info("!!!event: {}".format(event))
     else:
         if expire_time is not None and expired(expire_time):
             data = login(event.get("token"))
@@ -26,7 +24,6 @@ def lambda_handler(event, context):
                 event.get("user_id"),
                 data
             )
-    logging.info("!!!next is eval()")
     eval(event.get("vmc_command"))(event)
 
 def expired(expire_time):
@@ -34,7 +31,6 @@ def expired(expire_time):
     return now > expire_time
 
 def list_sddcs(event):
-    logging.info("!!!here is list_sddcs")
     list = sddc_list(
         event.get("access_token"), 
         event.get("org_id")
