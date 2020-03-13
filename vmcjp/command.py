dd4840fdcc5fcf5d47880cb36ce3251d0526d193
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-14 00:28:12 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -1,15 +1,15 @@
 import time
-#import logging
+import logging
 
 from vmcjp.utils import cmd_const
 from vmcjp.slack.db import write_cred_db
 from vmcjp.vmc.vmc_client import login, sddc_list
 
-#logger = logging.getLogger()
-#logger.setLevel(logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-#    logging.info(event)
+    logging.info(event)
     expire_time = event.get("expire_time")
     command = event.get("vmc_command")
     
@@ -37,7 +37,9 @@ def expired(expire_time):
     return now > expire_time
 
 def list_sddcs(event):
-    return sddc_list(
+    list = sddc_list(
         event.get("access_token"), 
         event.get("org_id")
     )
+    logging.info(list)
+    return list
