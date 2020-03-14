09ca5651557ef7951e66205b38b3feb3aafb2ad7
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-14 10:14:31 +0900

    Update lambdautils.py

--- vmcjp/utils/lambdautils.py
+++ vmcjp/utils/lambdautils.py
@@ -1,7 +1,11 @@
 import json
+import logging
 
 from botocore.session import Session
 from botocore.config import Config
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def call_lambda(function, data):
     s = Session()
@@ -16,6 +20,7 @@ def call_lambda(function, data):
     )
 
 def call_lambda_async(function, data):
+    logging.info("!!!call_lambda: {}, {}".format(function, data))
     s = Session()
     clientLambda = s.create_client(
         "lambda", 
