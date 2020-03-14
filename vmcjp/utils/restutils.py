0fc1029e5133b22aa9fe652b02145acc912ee888
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-14 18:33:36 +0900

    Update restutils.py

--- vmcjp/utils/restutils.py
+++ vmcjp/utils/restutils.py
@@ -1,1 +1,23 @@
-aaa
+import requests
+import json
+
+def post_request(url, headers, params):
+  response = requests.post(
+    url,
+    headers=headers,
+    params=params
+  )
+  if response is not None:
+    data = response.json()
+    if response.status_code == 200:
+      return data
+
+def get_request(url, headers):
+  response = requests.get(
+    url,
+    headers=headers
+  )
+  if response is not None:
+    data = response.json()
+    if response.status_code == 200:
+      return data
