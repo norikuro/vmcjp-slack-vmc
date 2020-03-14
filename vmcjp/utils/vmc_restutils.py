d0e1fdd506085e05572e480f1b2e041c0803e29d
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-15 00:07:15 +0900

    Update vmc_restutils.py

--- vmcjp/utils/vmc_restutils.py
+++ vmcjp/utils/vmc_restutils.py
@@ -3,26 +3,32 @@ import urllib.request
 import json
 import logging
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
-
-def post_request(url, headers, params):
+def post_request(url, headers, query=None, params=None):
+  if query is not None:
+    query = urllib.parse.urlencode(query)
+    url = "{}?{}".format(url, query)
+  if params is not None:
+    data = json.dumps(params).encode("utf-8")
+    
   request = urllib.request.Request(
     url,
-    data=json.dumps(params).encode("utf-8"), 
+    data=data, 
     headers=headers
   )
-  response = urllib.request.urlopen(request)
+#  response = urllib.request.urlopen(request)
+  with urllib.request.urlopen(request) as response:
+    data = response.read().decode("utf-8")
 #  response = requests.post(
 #    url,
 #    headers=headers,
 #    params=params
 #  )
-  logging.info(response.text)
-  if response is not None:
-    data = response.json()
-    if response.status_code == 200:
-      return data
+#  logging.info(response.text)
+#  if response is not None:
+#    data = response.json()
+#    if response.status_code == 200:
+#      return data
+  return data
 
 def get_request(url, headers):
   response = requests.get(
