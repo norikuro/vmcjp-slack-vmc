38f9f0d34500f794114a04cd8188937471f5da21
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-23 20:58:59 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -1,12 +1,12 @@
 import time
-import logging
+#import logging
 
 from vmcjp.utils import cmd_const
 from vmcjp.slack.db import write_cred_db
 from vmcjp.vmc.vmc_client import login, sddc_list, token_validation, get_sddcs, get_sddclimit, get_aws_region, get_connected_accounts, get_vpc_map
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+#logger = logging.getLogger()
+#logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
 #    logging.info(event)
@@ -133,11 +133,8 @@ def list_subnet(event):
         event.get("region"),
     )
     vpc_map = data.get("vpc_map")
-    logging.info("!!! vpc_map -> {}".format(vpc_map))
-    logging.info("!!! event vpc_id -> {}".format(event.get("vpc_id")))
     vpc = vpc_map.get(event.get("vpc_id"))
     subnets = vpc.get("subnets")
-    logging.info("!!! subnets -> {}".format(subnets))
     
     return [
         {
