e03e17ba949c4c6aebfe0c3f67d3c4cb5383da34
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-23 15:44:14 +0900

    Update vmc_client.py

--- vmcjp/vmc/vmc_client.py
+++ vmcjp/vmc/vmc_client.py
@@ -1,6 +1,6 @@
 import json
 import time
-#import logging
+import logging
 
 from vmcjp.utils.vmc_restutils import post_request, get_request
 
@@ -8,8 +8,8 @@ LOGIN_URL = "https://console.cloud.vmware.com/csp/gateway"
 VMC_URL = "https://vmc.vmware.com/vmc/api"
 HEADERS = {"Content-Type": "application/json"}
 
-#logger = logging.getLogger()
-#logger.setLevel(logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def update_headers(access_token):
     headers = {"csp-auth-token": access_token}
@@ -131,4 +131,5 @@ def get_vpc_map(access_token, org_id, linked_account_id, region):
         params = params
     )
 
+    logging.info("!!! data = {}".format(data))
     return data
