392ff9f9db4fce23fef4b8765b83c2842bebb063
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-29 11:32:26 +0900

    Update vmc_client.py

--- vmcjp/vmc/vmc_client.py
+++ vmcjp/vmc/vmc_client.py
@@ -164,24 +164,23 @@ def get_vpc_map(access_token, org_id, linked_account_id, region):
     return data
 
 def create_sddc(
-    access_token#,
+    access_token,
+    org_id, 
+    link_aws, 
+    connected_account_id=None, 
+    customer_subnet_id=None, 
+    deployment_type=None, 
+    host_instance_type=None, 
+    sddc_name, 
+    num_hosts, 
+    provider, 
+    region, 
+    sddc_type=None, 
+    size, 
+    storage_capacity=None, 
+    vpc_cidr
 ):
-    print("something")
-#    org_id, 
-#    link_aws, 
-#    connected_account_id=None, 
-#    customer_subnet_id=None, 
-#    deployment_type=None, 
-#    host_instance_type=None, 
-#    sddc_name, 
-#    num_hosts, 
-#    provider, 
-#    region, 
-#    sddc_type=None, 
-#    size, 
-#    storage_capacity=None, 
-#    vpc_cidr
-#):
+    print("!! something")
 #    uri = "/orgs/{}/sddcs".format(org_id)
 #    
 #    params = {
