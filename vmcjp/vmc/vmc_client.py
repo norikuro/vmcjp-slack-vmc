786e7086d2294c0186f05444c182970034452730
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-29 12:02:13 +0900

    Update vmc_client.py

--- vmcjp/vmc/vmc_client.py
+++ vmcjp/vmc/vmc_client.py
@@ -168,9 +168,9 @@ def create_sddc(
     org_id, 
     link_aws,
     connected_account_id=None, 
-    customer_subnet_id=None#, 
+    customer_subnet_id=None, 
+    deployment_type=None#, 
 ):
-#    deployment_type=None, 
 #    host_instance_type=None, 
 #    sddc_name, 
 #    num_hosts, 
