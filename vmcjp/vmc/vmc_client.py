0cba0f141ea550d830f14c787e4e97a31ba3bfa4
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-29 11:58:44 +0900

    Update vmc_client.py

--- vmcjp/vmc/vmc_client.py
+++ vmcjp/vmc/vmc_client.py
@@ -166,9 +166,9 @@ def get_vpc_map(access_token, org_id, linked_account_id, region):
 def create_sddc(
     access_token,
     org_id, 
-    link_aws#,
-):  
-#    connected_account_id=None, 
+    link_aws,
+    connected_account_id=None#, 
+):
 #    customer_subnet_id=None, 
 #    deployment_type=None, 
 #    host_instance_type=None, 
