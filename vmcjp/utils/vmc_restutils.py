693489e47fc3ab790714eab45e5866ca2f817667
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-30 15:41:13 +0900

    Update vmc_restutils.py

--- vmcjp/utils/vmc_restutils.py
+++ vmcjp/utils/vmc_restutils.py
@@ -1,55 +1,64 @@
 import urllib.request
 import urllib.error
+import requests
 import json
 import logging
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-def post_request(url, headers, query=None, params=None):
-  if query is not None:
-    query = urllib.parse.urlencode(query)
-    url = "{}?{}".format(url, query)
-  if params is not None:
-    data = json.dumps(params).encode("utf-8")
-  else:
-    data = None
+def post_request(url, headers, params=None, data=None):
+#  if query is not None:
+#    query = urllib.parse.urlencode(query)
+#    url = "{}?{}".format(url, query)
+#  if params is not None:
+#    data = json.dumps(params).encode("utf-8")
+#  else:
+#    data = None
 
-  request = urllib.request.Request(
-    url,
-    method = "POST",
-    data=data, 
-    headers=headers
-  )
+  if data is not None:
+    data = json.dumps(data).encode("utf-8")
+      
+#  request = urllib.request.Request(
+#    url,
+#    method = "POST",
+#    data=data, 
+#    headers=headers
+#  )
   
   try:
+    response = requests.post(
+      url,
+      headers=headers,
+      params=params,
+      data=data
+    )
+    logging.info(response.json())
+#    with urllib.request.urlopen(request) as response:
+#      data = json.loads(response.read().decode("utf-8"))
+#      return data
     
-    with urllib.request.urlopen(request) as response:
-      logging.info(response)
-      data = json.loads(response.read().decode("utf-8"))
-      return data
+#  except urllib.error.HTTPError as err:
+#    logging.info("!!! err,, {}".format(err.reason))
+#    logging.info("!!! data,, {}".format(data))
     
-  except urllib.error.HTTPError as err:
-    logging.info("!!! err,, {}".format(err.reason))
-    logging.info("!!! data,, {}".format(data))
-    
-    if err.code in [400, 401, 403, 404]:
-      if data.get("error_messages") is not None:
-        raise Exception(data.get("error_messages")[0])
-      elif data.get("message") is not None:
-        raise Exception(data.get("message"))
-      else:
-        raise Exception("Failed to send/get request, something wrong!")
-    else:
-      raise Exception("Failed to send/get request, something wrong!")
+#    if err.code in [400, 401, 403, 404]:
+#      if data.get("error_messages") is not None:
+#        raise Exception(data.get("error_messages")[0])
+#      elif data.get("message") is not None:
+#        raise Exception(data.get("message"))
+#      else:
+#        raise Exception("Failed to send/get request, something wrong!")
+#    else:
+#      raise Exception("Failed to send/get request, something wrong!")
       
-  except urllib.error.URLError as err:
+#  except urllib.error.URLError as err:
     
-    reason = err.reason
-    if reason is not None:
-      raise Exception(reason)
-    else:
-      raise Exception("Failed to send/get request, something wrong!")
+#    reason = err.reason
+#    if reason is not None:
+#      raise Exception(reason)
+#    else:
+#      raise Exception("Failed to send/get request, something wrong!")
 
 def get_request(url, headers, params=None):
   if params is not None:
