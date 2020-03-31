c968c445717bac9ddb36eb63f8fff15a72188203
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-03-31 16:21:25 +0900

    Update vmc_restutils.py

--- vmcjp/utils/vmc_restutils.py
+++ vmcjp/utils/vmc_restutils.py
@@ -8,24 +8,9 @@ import json
 #logger.setLevel(logging.INFO)
 
 def post_request(url, headers, params=None, data=None):
-#  if query is not None:
-#    query = urllib.parse.urlencode(query)
-#    url = "{}?{}".format(url, query)
-#  if params is not None:
-#    data = json.dumps(params).encode("utf-8")
-#  else:
-#    data = None
-
   if data is not None:
     data = json.dumps(data).encode("utf-8")
       
-#  request = urllib.request.Request(
-#    url,
-#    method = "POST",
-#    data=data, 
-#    headers=headers
-#  )
-  
   try:
     response = requests.post(
       url,
@@ -56,32 +41,6 @@ def post_request(url, headers, params=None, data=None):
     else:
       raise Exception("Something wrong!")
     
-#    with urllib.request.urlopen(request) as response:
-#      data = json.loads(response.read().decode("utf-8"))
-#      return data
-    
-#  except urllib.error.HTTPError as err:
-#    logging.info("!!! err,, {}".format(err.reason))
-#    logging.info("!!! data,, {}".format(data))
-    
-#    if err.code in [400, 401, 403, 404]:
-#      if data.get("error_messages") is not None:
-#        raise Exception(data.get("error_messages")[0])
-#      elif data.get("message") is not None:
-#        raise Exception(data.get("message"))
-#      else:
-#        raise Exception("Failed to send/get request, something wrong!")
-#    else:
-#      raise Exception("Failed to send/get request, something wrong!")
-      
-#  except urllib.error.URLError as err:
-    
-#    reason = err.reason
-#    if reason is not None:
-#      raise Exception(reason)
-#    else:
-#      raise Exception("Failed to send/get request, something wrong!")
-
 def get_request(url, headers, params=None):
   if params is not None:
     query = urllib.parse.urlencode(params)
