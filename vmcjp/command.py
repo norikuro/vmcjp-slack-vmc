6ad8a5d9421cd188a83d2ad80b16f76623a6231f
Noritaka Kuroiwa <nkuroiwa@vmware.com>
2020-04-09 12:49:02 +0900

    Update command.py

--- vmcjp/command.py
+++ vmcjp/command.py
@@ -1,12 +1,12 @@
 import time
-#import logging
+import logging
 
 from vmcjp.utils import cmd_const
 from vmcjp.slack.db import write_cred_db
 from vmcjp.vmc.vmc_client import login, sddc_list, token_validation, get_sddcs, get_sddclimit, get_aws_region, get_connected_accounts, get_vpc_map, sddc_creation
 
-#logger = logging.getLogger()
-#logger.setLevel(logging.INFO)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
 #    logging.info(event)
@@ -166,5 +166,5 @@ def create_sddc(event):
         event.get("sddc_type"), 
         event.get("storage_capacity")
     )
-    
+    logging.info("!!! task data, {}".format(data))
     return data.get("id")
